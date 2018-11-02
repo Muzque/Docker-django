e23c5e1dcf48341cd43f0bdd36d380bab3b9b71b
Muzque <zerosky1943@gmail.com>
2018-11-02 09:59:57 +0800

    replace running git at .sh

--- django/build.sh
+++ django/build.sh
@@ -1,3 +1,10 @@
+git init
+git remote add origin clone https://github.com/Muzque/django-youtube.git
+git fetch
+git branch master origin/master
+git checkout master
+pip install --upgrade pip
+pip install -r requirements.txt
 python wait_for_mysql.py
 python manage.py collectstatic --no-input
 python manage.py makemigrations
